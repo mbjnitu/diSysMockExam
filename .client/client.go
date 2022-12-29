01a1e9b2bda0611cfa2a65459cc99437339ba86d
mbjn <mbjn@itu.dk>
2022-12-29 14:17:32 +0100

    Implemented client with increment, and server with receive increment methods.

---
+++ .client/client.go
@@ -0,0 +1,123 @@
+package main
+
+import (
+	"bufio"
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+
+	gRPC "github.com/mbjnitu/diSysMockExam/proto"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+// Same principle as in client. Flags allows for user specific arguments/values
+var clientsName = flag.String("name", "default", "Senders name")
+var serverPort = flag.String("server", "5400", "Tcp server")
+
+var server gRPC.TemplateClient  //the server
+var ServerConn *grpc.ClientConn //the server connection
+
+func main() {
+	//parse flag/arguments
+	flag.Parse()
+
+	fmt.Println("--- CLIENT APP ---")
+
+	//log to file instead of console
+	//f := setLog()
+	//defer f.Close()
+
+	//connect to server and close the connection when program closes
+	fmt.Println("--- join Server ---")
+	ConnectToServer()
+	defer ServerConn.Close()
+
+	//start the listening to user calls
+	parseInput()
+}
+
+func ConnectToServer() {
+
+	//dial options
+	//the server is not using TLS, so we use insecure credentials
+	//(should be fine for local testing but not in the real world)
+	var opts []grpc.DialOption
+	opts = append(opts, grpc.WithBlock(), grpc.WithTransportCredentials(insecure.NewCredentials()))
+
+	//dial the server, with the flag "server", to get a connection to it
+	log.Printf("client %s: Attempts to dial on port %s\n", *clientsName, *serverPort)
+	conn, err := grpc.Dial(fmt.Sprintf(":%s", *serverPort), opts...)
+	if err != nil {
+		log.Printf("Fail to Dial : %v", err)
+		return
+	}
+
+	// makes a client from the server connection and saves the connection
+	// and prints rather or not the connection was is READY
+	server = gRPC.NewTemplateClient(conn)
+	ServerConn = conn
+	log.Println("the connection is: ", conn.GetState().String())
+}
+
+func parseInput() {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Ready for command:")
+	fmt.Println("--------------------")
+
+	//Infinite loop to listen for clients input.
+	for {
+		fmt.Print("-> ")
+
+		//Read input into var input and any errors into err
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
+		input = strings.TrimSpace(input) //Trim input
+
+		if !conReady(server) {
+			log.Printf("Client %s: something was wrong with the connection to the server :(", *clientsName)
+			continue
+		}
+
+		//Convert string to int64, return error if the int is larger than 32bit or not a number
+		val, err := strconv.ParseInt(input, 10, 64)
+		if err == nil {
+			incrementVal(val)
+			continue
+		}
+		else {
+			fmt.Print("Increment only acceps a number")
+		}
+	}
+}
+
+func incrementVal(val int64) {
+	//create amount type
+	amount := &gRPC.Amount{
+		ClientName: *clientsName,
+		Value:      val, //cast from int to int32
+	}
+
+	//Make gRPC call to server with amount, and recieve acknowlegdement back.
+	ack, err := server.Increment(context.Background(), amount)
+	if err != nil {
+		log.Printf("Client %s: no response from the server, attempting to reconnect", *clientsName)
+		log.Println(err)
+	}
+
+	// check if the server has handled the request correctly
+	if ack.NewValue >= val {
+		fmt.Printf("Success, the new value is now %d\n", ack.NewValue)
+	} else {
+		// something could be added here to handle the error
+		// but hopefully this will never be reached
+		fmt.Println("Oh no something went wrong :(")
+	}
+}
